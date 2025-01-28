c73cb896ddde1f8d771f698a4a25a76f0efc9aec
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2025-01-28 18:47:27 -0300

    initial commit

---
+++ iStudy-services/auth-ms/src/main/java/com/io/github/abeatrizsc/auth_ms/AuthMsApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.abeatrizsc.auth_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMsApplication.class, args);
+	}
+
+}
