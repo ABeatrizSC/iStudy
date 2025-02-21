dc4d2c95c8618a58d7f0d9b8d84b1c96911670cc
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2025-02-21 14:33:18 -0300

    study-gamification-ms initial commit

---
+++ iStudy-services/study-gamification-ms/src/test/java/io/github/abeatrizsc/study_gamification_ms/StudyGamificationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abeatrizsc.study_gamification_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGamificationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
