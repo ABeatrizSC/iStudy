69bf41b1683dd7af471c2c8d2e1efc9529257c33
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2025-03-25 00:26:38 -0300

    fix: add use client that was missing

--- istudy-app/src/components/Modal/UpdateTopicModal.tsx
+++ istudy-app/src/components/Modal/UpdateTopicModal.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
--- istudy-app/src/components/Sidebar.tsx
+++ istudy-app/src/components/Sidebar.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import React, { useState } from "react";
 import theme from '@/resources/assets/styles/Theme';
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { BookCheckIcon, Calendar, ChevronDown, ClipboardList, Gamepad2, Home, LogOut, Menu, SquareGanttChart, StickyNote, Timer } from "lucide-react";
 import istudyLogo from '../resources/assets/images/iStudyLogo.png';
-import {Button} from '@/components/Button';
+import { Button } from '@/components/Button';
 import Link from "next/link";
 import { useAuthService } from "@/resources/services/auth-user/authentication.service";
 
